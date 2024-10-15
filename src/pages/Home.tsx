513ab3c8857a538c8a1abc831560eb22efb348fa
kasperfryd <kfry@techcollege.dk>
2024-10-15 08:36:13 +0200

    fixed home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,7 @@
-import { useContext, useEffect, useState } from "react";
-import supabase from "../utils/supabase";
-import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { useContext } from "react";
 import { Note } from "../components/Note/Note";
 import { Divider } from "@nextui-org/react";
-
-import { getAllNotes } from "../queries/getAllNotes";
 import Reactlogo from "../assets/react.svg";
-
-export const Home = () => {
-  const [dataArr, setDataArr] = useState<any>();
-
-  const data = getAllNotes();
-
-  /*   useEffect(() => {
-    console.log(dataArr);
-  }, [dataArr]); */
-
-  return (
-    <>
-      {data?.data?.map((item: any, index: number) => {
 import { HomeContext } from "../context/HomepageContext";
 
 export const Home = () => {
